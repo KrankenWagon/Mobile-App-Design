7f645cea05778259b9c567fa3cd30029f257a4c6
Caleb Barnhart <barnhartcaleb16@gmail.com>
2019-04-04 17:45:19 -0400

    In Class Work start page and block

--- Code/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.myapplication;
 
+/*Programmer:     Caleb Barnhart
+        Program Name:   Blood Bowl Calculator */
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void block(View view){
+
+    }
+
+    public void skills(View view){
+
+    }
+
+    public void calculateBlock(View view){
+
     }
 }
